2491e1eb5dcd19f899336cafe573ec1172f8bdbf
DVMelnikov <MelnikovDV@bcs.ru>
2025-11-11 22:00:52 +0400

    Added Lesson 1 homework

---
+++ Lesson 2.py
@@ -0,0 +1,46 @@
+# Раздел: Объявление переменных
+my_city = "Saratov"
+print(my_city)
+
+# Раздел: Операции сравнения
+a, b = 15, 10
+result = a > b
+print(result)
+
+# Раздел: Преобразование типов данных
+price = "150"
+int_price = int(price)
+result_price = int_price + 50
+print(result_price)
+
+# Раздел: Арифметические операции
+x, y = 20, 6
+result = x // y
+print(result)
+
+# Раздел: Типы данных
+pi = 3.14
+name = "Denis"
+boolean = False
+print(type(pi), type(name), type(boolean))
+
+# Раздел: Именование переменных
+number_of_students = 25
+print(number_of_students)
+
+# Раздел: Переопределение переменной
+score = 100
+score = 150
+print(score)
+
+# Раздел: Классификация по типам данных
+meaning_1 = 100
+meaning_2 = "100"
+meaning_3 = 100.5
+print("Значение " + str(meaning_1) + " относится к типу " + str(type(meaning_1)))
+print("Значение " + str(meaning_2) + " относится к типу " + str(type(meaning_2)))
+print("Значение " + str(meaning_3) + " относится к типу " + str(type(meaning_3)))
+
+
+
+

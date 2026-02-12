af92bcb02297e17c21500eb5378b2bb5b6016e16
Alena <melnikovaalenav@gmail.com>
2026-02-12 23:50:57 +0400

    Add Lesson 1-2 homework (pytest)

---
+++ homework_pytest/test_lesson_1_2.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+import pytest
+
+class TestExample:
+
+    USERNAME = ('xpath', '//input[@id="userName"]')
+    EMAIL = ('xpath', '//input[@id="userEmail"]')
+    ADDRESS_CURR = ('xpath', '//textarea[@id="currentAddress"]')
+    ADDRESS_PERM = ('xpath', '//textarea[@id="permanentAddress"]')
+    BUTTON = ('xpath', '//button[@id="submit"]')
+    OUTPUT = ('xpath', '//div[@id="output"]')
+
+    def setup_method(self):
+        options = webdriver.ChromeOptions()
+        options.page_load_strategy = 'eager'
+        options.add_argument('window-size=1900x1080')
+        self.driver = webdriver.Chrome(options=options)
+        self.driver.get("https://demoqa.com/text-box")
+
+    @pytest.mark.smoke
+    def test_url(self):
+        assert self.driver.current_url == "https://demoqa.com/text-box", 'Неверный url'
+
+    @pytest.mark.regres
+    def test_input_field(self):
+        self.driver.find_element(*self.USERNAME).send_keys('Denis')
+        self.driver.find_element(*self.EMAIL).send_keys('Denis@mail.com')
+        self.driver.find_element(*self.ADDRESS_CURR).send_keys('Test1234')
+        self.driver.find_element(*self.ADDRESS_PERM).send_keys('Test1234')
+        self.driver.find_element(*self.BUTTON).click()
+        assert "Denis" in self.driver.find_element(*self.OUTPUT).text, 'Нет сохранения инпутов'
+
+    def teardown_method(self):
+        self.driver.quit()
+
+
